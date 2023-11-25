91e7d0aa30a56ae3179e1b76769432b7afeebc73
Welton <weltonrdesigner@gmail.com>
2023-11-25 17:11:48 -0300

    Initial Commit

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,25 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // Herdando da classe "Smartphone"
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Adicionando propriedades específicas do iPhone
+        public string Modelo { get; set; }
+        public string SistemaOperacional { get; set; }
+
+        // Construtor que recebe parâmetros específicos do iPhone
+        public Iphone(string numero, string modelo, string sistemaOperacional) : base(numero)
+        {
+            Modelo = modelo;
+            SistemaOperacional = sistemaOperacional;
+        }
+
+        // Sobrescrevendo o método InstalarAplicativo
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no iPhone {Modelo} com SO {SistemaOperacional}");
+        }
+
+        // Outros métodos específicos do iPhone, se necessário
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,23 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herdando da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Adicionando propriedade específica do Nokia
+        public string Modelo { get; set; }
+
+        // Construtor que recebe parâmetros específicos do Nokia
+        public Nokia(string numero, string modelo) : base(numero)
+        {
+            Modelo = modelo;
+        }
+
+        // Sobrescrevendo o método InstalarAplicativo
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia {Modelo}");
+        }
+
+        // Outros métodos específicos do Nokia, se necessário
     }
 }
